930bf2f0102637e47604bd32fd48e5e2cec9a3ed
khuattiendat <khuattiendat1@gmail.com>
2024-10-19 14:57:22 +0700

    test

---
+++ client/src/container/Novaup/StatusData.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DragAndDropTable from "../table/DragTable";
+import {PageHeader} from "../../components/page-headers/page-headers";
+
+const StatusData = () => {
+    return (
+        <div>
+            <PageHeader ghost title="Trạng thái data" />
+            <DragAndDropTable />
+        </div>
+    )
+}
+export default StatusData;
--- client/src/routes/admin/index.js
+++ client/src/routes/admin/index.js
@@ -1,6 +1,6 @@
-import React, { Suspense, lazy } from 'react';
-import { Spin } from 'antd';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import React, {Suspense, lazy} from 'react';
+import {Spin} from 'antd';
+import {Switch, Route, useRouteMatch} from 'react-router-dom';
 
 import Dashboard from './dashboard';
 import Ecommerce from './ecommerce';
@@ -10,6 +10,7 @@ import Widgets from './widgets';
 import Features from './features';
 import Axios from './axios';
 import Gallery from './gallery';
+import Novaup from "./Novaup";
 import withAdminLayout from '../../layout/withAdminLayout';
 
 const Projects = lazy(() => import('./projects'));
@@ -29,42 +30,46 @@ const Kanban = lazy(() => import('../../container/kanban/Index'));
 const Task = lazy(() => import('../../container/task/Index'));
 
 function Admin() {
-  const { path } = useRouteMatch();
+    const {path} = useRouteMatch();
+    console.log(path)
 
-  return (
-    <Switch>
-      <Suspense
-        fallback={
-          <div className="spin">
-            <Spin />
-          </div>
-        }
-      >
-        <Route path={path} component={Dashboard} />
-        <Route path={`${path}/ecommerce`} component={Ecommerce} />
-        <Route path={`${path}`} component={Pages} />
-        <Route path={`${path}`} component={Features} />
-        <Route path={`${path}`} component={Axios} />
-        <Route path={`${path}/users`} component={Users} />
-        <Route path={`${path}/gallery`} component={Gallery} />
-        <Route path={`${path}/project`} component={Projects} />
-        <Route path={`${path}/calendar`} component={Calendars} />        
-        <Route path={`${path}/app/kanban`} component={Kanban} />
-        <Route path={`${path}/email/:page`} component={Inbox} />
-        <Route path={`${path}/firestore`} component={Firebase} />
-        <Route path={`${path}/main/chat`} component={Chat} />
-        <Route path={`${path}/profile/myProfile`} component={Myprofile} />
-        <Route path={`${path}/app/to-do`} component={ToDo} />
-        <Route path={`${path}/app/note`} component={Note} />
-        <Route path={`${path}/app/task`} component={Task} />
-        <Route path={`${path}/contact/list`} component={Contact} />
-        <Route path={`${path}/contact/grid`} component={ContactGrid} />
-        <Route path={`${path}/contact/addNew`} component={ContactAddNew} />
-        <Route path={`${path}/app/calendar`} component={Calendar} />
-        <Route path={`${path}/widgets`} component={Widgets} />
-      </Suspense>
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Suspense
+                fallback={
+                    <div className="spin">
+                        <Spin/>
+                    </div>
+                }
+            >
+                <Route path={path} component={Dashboard}/>
+                <Route path={`${path}/dashboard`} component={Dashboard}/>
+                <Route path={`${path}/ecommerce`} component={Ecommerce}/>
+                <Route path={`${path}`} component={Pages}/>
+                <Route path={`${path}`} component={Features}/>
+                <Route path={`${path}`} component={Axios}/>
+                <Route path={`${path}/users`} component={Users}/>
+                <Route path={`${path}/gallery`} component={Gallery}/>
+                <Route path={`${path}/project`} component={Projects}/>
+                <Route path={`${path}/calendar`} component={Calendars}/>
+                <Route path={`${path}/app/kanban`} component={Kanban}/>
+                <Route path={`${path}/email/:page`} component={Inbox}/>
+                <Route path={`${path}/firestore`} component={Firebase}/>
+                <Route path={`${path}/main/chat`} component={Chat}/>
+                <Route path={`${path}/profile/myProfile`} component={Myprofile}/>
+                <Route path={`${path}/app/to-do`} component={ToDo}/>
+                <Route path={`${path}/app/note`} component={Note}/>
+                <Route path={`${path}/app/task`} component={Task}/>
+                <Route path={`${path}/contact/list`} component={Contact}/>
+                <Route path={`${path}/contact/grid`} component={ContactGrid}/>
+                <Route path={`${path}/contact/addNew`} component={ContactAddNew}/>
+                <Route path={`${path}/app/calendar`} component={Calendar}/>
+                <Route path={`${path}/widgets`} component={Widgets}/>
+                {/**/}
+                <Route path={`${path}/novaup`} component={Novaup}/>
+            </Suspense>
+        </Switch>
+    );
 }
 
 export default withAdminLayout(Admin);
